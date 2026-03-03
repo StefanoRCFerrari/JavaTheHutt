cedb71ec65f3bb352791828904e4520e1311ee77
Stéfano Ferrari <stefanorcferrari@gmail.com>
2026-03-03 08:55:17 -0300

    Add files via upload

---
+++ Cafezinho0303/src/cafezinho0303/Cafezinho0303.java
@@ -0,0 +1,16 @@
+package cafezinho0303;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Cafezinho0303 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String teste = JOptionPane.showInputDialog("Oi");
+        System.out.println(teste);
+    
+        //nextInt e etc para scanner
+    }
+    
+}
